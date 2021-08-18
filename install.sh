b904db62aab48681ba3dff69eb264d6b70927407
lwqlwq60 <liuweiqian01@163.com>
2021-08-18 12:58:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ if [ `grep -c "4243" /lib/systemd/system/docker.service` -eq '0' ]; then
          service docker restart
 fi
 
-docker run -p 51888:80 --name="crust-monitor-node" --mount source=/opt/crust,target=/opt/crust --restart always --add-host=host.docker.internal:host-gateway -d lwqlwq60/crust-monitor-node:latest 
+docker run -p 51888:80 --name="crust-monitor-node" --v /opt/crust:/opt/crust --restart always --add-host=host.docker.internal:host-gateway -d lwqlwq60/crust-monitor-node:latest 
